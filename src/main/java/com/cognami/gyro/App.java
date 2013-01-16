b86eeabf836141ec0a80568f7e63e00c2d8df9d9
Daniel Jue <djue@phy6.net>
2013-01-16 10:51:41 -0500

    First commit
    Basic maven project shell, pre-clojure

---
+++ src/main/java/com/cognami/gyro/App.java
@@ -0,0 +1,13 @@
+package com.cognami.gyro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
